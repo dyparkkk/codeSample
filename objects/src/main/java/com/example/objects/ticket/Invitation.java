4bfc5d05b9cebf8421842eb23b7778504e521afd
dyparkkk <dkdlel1z@naver.com>
2022-04-19 10:37:52 +0900

    init objects

---
+++ objects/src/main/java/com/example/objects/ticket/Invitation.java
@@ -0,0 +1,7 @@
+package com.example.objects.ticket;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ objects/src/main/java/com/example/objects/ticket/Ticket.java
@@ -0,0 +1,9 @@
+package com.example.objects.ticket;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
