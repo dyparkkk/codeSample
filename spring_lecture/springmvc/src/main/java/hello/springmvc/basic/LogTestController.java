b55999603acf5bc69ec2d52d8331678fbba05de3
dyparkkk <dkdlel1z@naver.com>
2022-05-24 12:20:43 +0900

    mvc 섹션 6

--- spring_lecture/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ spring_lecture/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -3,9 +3,7 @@ package hello.springmvc.basic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class LogTestController {
--- spring_lecture/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ spring_lecture/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -1,15 +1,53 @@
-//package hello.springmvc.basic.request;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//@Slf4j
-//@Controller
-//public class RequestBodyStringController {
-//
-//    @PostMapping("/request-body-string-v1")
-//    public void requestBodyString(HttpServletRequest)
-//}
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestBodyStringController {
+
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyString(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        ServletInputStream inputStream = req.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        resp.getWriter().write(messageBody);
+    }
+
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer writer) throws IOException {
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        writer.write(messageBody);
+    }
+
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+        String message = httpEntity.getBody();
+        log.info("messageBody = {}", message);
+
+        return new HttpEntity<>("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String message) {
+        log.info("messageBody = {}", message);
+        return "ok";
+    }
+}
